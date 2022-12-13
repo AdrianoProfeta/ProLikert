101bdff1ec018c8b4403812f8fa64e41d7754009
AdrianoProfeta <a.profeta@dil-ev.de>
2022-12-13 19:36:51 +0100

    second test

---
+++ R/hello.R
@@ -0,0 +1,30 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+hello <- function() {
+  print("Hello, world!")
+}
+# Simulate and plot data
+# Jeff Oliver
+# jcoliver@arizona.edu
+# 2021-03-23
+
+# Simulate predictor variable
+x <- rnorm(n = 100)
+# Simulate response variable with some noise
+y <- 2 * x + rnorm(n = 100, sd = 0.2)
+
+# Plot the data
+plot(x = x, y = y)
